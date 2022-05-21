bc55f4c55381a05c304c7d06585259c3b3777d02
tandingming <2084954083.@qq.com>
2022-05-21 22:47:30 +0800

    Initial commit

---
+++ src/main/java/com/mycrm/crm/setting/controller/UserContorller.java
@@ -0,0 +1,16 @@
+package com.mycrm.crm.setting.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : TanDingMing
+ * @version :
+ * @Project : crm2008
+ * @Package : com.mycrm.crm.setting.controller
+ * @ClassName : .java
+ * @createTime : 2022/5/21 22:42
+ * @Email :
+ * @Description :
+ */
+public class UserContorller {
+}
